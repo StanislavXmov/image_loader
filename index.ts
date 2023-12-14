a88d8dc3aeb248dc8ae3c4a8755bf66e27357608
stanislav <xiiiovart@gmail.com>
2023-12-14 20:09:56 +0300

    init

---
+++ index.ts
@@ -0,0 +1,48 @@
+import express from 'express';
+import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
+
+const app = express();
+
+const catalog = '../static/images';
+
+const upload = multer({dest: catalog});
+const type = upload.array('filedata', 12);
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/upload', type, (req, res) => {
+	// console.log('files', req.files);
+  const files = req.files as Express.Multer.File[];
+	let srcArr = [];
+
+  if (files) {
+    for (let i = 0; i < files.length; i++) {
+      const tmpPath = files[i].path;
+      const targetPath = path.join(files[i].destination, files[i].originalname);
+      const src = fs.createReadStream(tmpPath);
+      const dest = fs.createWriteStream(targetPath);
+
+      src.pipe(dest);
+      srcArr.push({src, tmpPath});
+    }
+
+    for (const s of srcArr) {
+      s.src.on('end', function() {
+        fs.unlink(s.tmpPath, (err) => {
+        if (err) throw err;
+        });  
+      });
+    }
+  }
+	res.end();  
+});
+
+
+
+app.listen(5000, () => console.log('Server started'));
